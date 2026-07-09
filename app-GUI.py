9ab4658fe2e890941732edcd4dab58ec00672ae7
BadalSuyog <badalsuyog11@gmail.com>
2026-07-09 13:11:47 -0500

    Add initial comments for app development
    Add initial comments outlining app goals and features.

---
+++ app-GUI.py
@@ -0,0 +1,2 @@
+## to do:
+##to build a interactive app, which for now should be able to show cool stuffs like temeprature, later can add something related to the plotting the curves as well, as a live view, which should be really cool.
